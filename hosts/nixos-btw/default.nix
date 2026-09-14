007418ca8897ec5c45605c85cd3a2a04797348d4
lightmon <lightmon5210@outlook.com>
2026-09-14 20:18:42 +0800

    fcitx5 enabled waylandfrontend

--- hosts/nixos-btw/default.nix
+++ hosts/nixos-btw/default.nix
@@ -115,6 +115,7 @@
     enable = true;
     type = "fcitx5";
     fcitx5 = {
+      waylandFrontend = true;
       addons = with pkgs; [
         qt6Packages.fcitx5-chinese-addons
         fcitx5-rime
